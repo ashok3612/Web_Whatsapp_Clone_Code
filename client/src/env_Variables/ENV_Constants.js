69d1b7e685cb672cb57e46a6eae2b033a3481783
Ashok Selvaraj <ashokvmit@gmail.com>
2021-03-13 13:56:20 +0530

    sourse update

--- client/src/env_Variables/ENV_Constants.js
+++ client/src/env_Variables/ENV_Constants.js
@@ -1,5 +1,8 @@
-export const serverHostostName = "https://clonewhatsapp2.herokuapp.com";
+//export const serverHostostName = "https://clonewhatsapp2.herokuapp.com";
+export const serverHostostName = "http://localhost:8000";
 
-export const apiURL = "https://clonewhatsapp2.herokuapp.com";
+//export const apiURL = "https://clonewhatsapp2.herokuapp.com";
+export const apiURL = "http://localhost:8000";
 
-export const googleClientID = "90784126384-0m624o5gp3om6g4c75i383tgtct1m6oe.apps.googleusercontent.com";
+//export const googleClientID = "90784126384-0m624o5gp3om6g4c75i383tgtct1m6oe.apps.googleusercontent.com";
+export const googleClientID = "90784126384-vc30f3gt89u994smtl5qg9ureghvm4ce.apps.googleusercontent.com";
