f0c90a70f3b2c65e2f319654f13bd01744e8b51d
Ashok Selvaraj <ashokvmit@gmail.com>
2021-03-13 13:42:50 +0530

    Host update commmit

--- client/src/env_Variables/ENV_Constants.js
+++ client/src/env_Variables/ENV_Constants.js
@@ -1,5 +1,5 @@
-export const serverHostostName = "http://localhost:8000";
+export const serverHostostName = "https://clonewhatsapp2.herokuapp.com";
 
-export const apiURL = "http://localhost:8000";
+export const apiURL = "https://clonewhatsapp2.herokuapp.com";
 
 export const googleClientID = "90784126384-0m624o5gp3om6g4c75i383tgtct1m6oe.apps.googleusercontent.com";
